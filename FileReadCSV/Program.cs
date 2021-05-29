fb4c2c57eb5b6d4f85c7f43f0680c38ff2d25e3f
BeHairedMan <kontakt@behairedman.com>
2021-05-29 10:48:08 +0200

    Add project files.

---
+++ FileReadCSV/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileReadCSV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
