9bbd05578954e71af7a542c36386d6f32c0b30a8
BeHairedMan <kontakt@behairedman.com>
2021-06-03 14:29:10 +0200

    Xlsx einlesen Session | OLEDB

--- FileReadCSV/Models/ExcelModel.cs
+++ FileReadCSV/Models/ExcelModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Data;
 using System.Text;
 using System.IO;
 using System.Xml;
@@ -13,6 +15,7 @@ namespace FileRead
         public string[] content { get; }
         public string[] header { get; }
 
+        //list<T> als datensatz attribut - content rausnehmen
 
         public ExcelModel(string filePath)
         {
@@ -21,14 +24,40 @@ namespace FileRead
 
             // this.content = System.IO.File.ReadAllLines(@$"{this.path}");
 
-            XmlDocument xmlDoc = new XmlDocument();
-            xmlDoc.Load(this.path);
-
 
         }
 
+        public void readxlsx() //Parameter durchdenken
+        {
+            ////https://www.codingame.com/playgrounds/9014/read-write-excel-file-with-oledb-in-c-without-interop
+            //string szConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\BastianHartmann\source\repos\behairedman\FileRead\FileReadCSV\xlsx\Uebungs1.xlsx; Extended Properties='Excel 12.0;'";
+
+            string path = @"C:\Users\BastianHartmann\source\repos\behairedman\FileRead\FileReadCSV\xlsx\Uebungs1 - Kopie.xlsx";
+            string connStr = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + path + ";Extended Properties=Excel 12.0;";
+
+            using (OleDbConnection connection = new(connStr))
+            {
+                connection.Open();
+
+                OleDbDataAdapter OleDBDataAdaper = new OleDbDataAdapter("select * from [Sheet1$]", connection);
+                DataSet excelDataSet = new DataSet();
+                OleDBDataAdaper.Fill(excelDataSet);
+
+                //Inhalt auf Excelmodel mappen
+                //schema anschauen um header zu generieren
+
+                foreach (DataRow row in excelDataSet.Tables[0].Rows)
+                {
+                    foreach (DataColumn column in excelDataSet.Tables[0].Columns)
+                    {
+                        Console.WriteLine(row[1].GetType());
+                    }
+                }
+            }
+
+        }
+
+
+
     }
-
-
-
 }
--- FileReadCSV/Program.cs
+++ FileReadCSV/Program.cs
@@ -1,6 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+
+
 using System.IO;
+using System.Text;
+using System.Threading.Tasks;
 using System.Xml;
+
 
 
 
@@ -8,8 +14,10 @@ namespace FileRead
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+
 
             //https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/file-system/how-to-read-from-a-text-file
 
@@ -36,17 +44,34 @@ namespace FileRead
             //Console.WriteLine($"{output.header[0]}, {output.header[1]}, {output.header[2]}");
             //Console.WriteLine($"{output.content[0]}");
 
-            // https://stackoverflow.com/questions/17795167/xml-loaddata-data-at-the-root-level-is-invalid-line-1-position-1
-            XmlDocument xmlDoc = new XmlDocument();
-            xmlDoc.LoadXml(@"C:\Users\BastianHartmann\source\repos\behairedman\FileRead\FileReadCSV\xlsx\Uebungs1.xlsx");
+            //// https://stackoverflow.com/questions/17795167/xml-loaddata-data-at-the-root-level-is-invalid-line-1-position-1
+            //XmlDocument xmlDoc = new XmlDocument();
+
+            //XmlDocument xmlDoc = new XmlDocument();
+            ////xmlDoc.Load(@"C:\Users\BastianHartmann\source\repos\behairedman\FileRead\FileReadCSV\xlsx\Uebungs1.xlsx\xl\sharedStrings.xml");
+
+            //xmlDoc.Load(@"C:\Users\BastianHartmann\source\repos\behairedman\FileRead\FileReadCSV\xlsx\xl\sharedStrings.xml");
+            //string xmlText = xmlDoc.InnerXml;
+
+            //DataSet ds = new DataSet();
+            //ds.ReadXml(@"C:\Users\BastianHartmann\source\repos\behairedman\FileRead\FileReadCSV\xlsx\xl\sharedStrings.xml");
+
+
+            //Console.WriteLine(ds.Tables[2]);
+
+            // Connect EXCEL sheet with OLEDB using connection string
+            // if the File extension is .XLS using below connection string
+            //In following sample 'szFilePath' is the variable for filePath
+            //string szConnectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;
+            //           "Data Source = '" + szFilePath + 
+            //           "';Extended Properties=\"Excel 8.0;HDR=YES;\"";
+
+            // if the File extension is .XLSX using below connection string
 
 
 
 
 
-
-
-            Console.ReadLine();
 
            
         }
